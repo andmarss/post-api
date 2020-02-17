329cd786f49f22fec5f825f2e98b774ce0d677ce
Андрей Марков <anrey-markov@mail.ru>
2020-02-17 21:35:39 +0300

    add response function to bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
 if (!function_exists('collect')) {
+    /**
+     * @param array $collection
+     * @return \App\System\Collection
+     */
     function collect(array $collection = []): \App\System\Collection
     {
         return new \App\System\Collection($collection);
@@ -9,6 +13,10 @@ if (!function_exists('collect')) {
 
 if (!function_exists('class_basename'))
 {
+    /**
+     * @param $class
+     * @return string
+     */
     function class_basename($class)
     {
         $class = is_object($class) ? get_class($class) : $class;
@@ -19,11 +27,28 @@ if (!function_exists('class_basename'))
 
 if (!function_exists('dd'))
 {
+    /**
+     * @param mixed ...$data
+     */
     function dd(...$data)
     {
         echo sprintf('<pre style="color: %s">', 'black');
             var_dump(...$data);
         echo '</pre>';
         die;
+    }
+}
+
+if (!function_exists('response'))
+{
+    /**
+     * @param string $content
+     * @param int $status
+     * @param array $headers
+     * @return \App\System\Response
+     */
+    function response(string $content = '', int $status = 200, array $headers = []): \App\System\Response
+    {
+        return new \App\System\Response($content, $status, $headers);
     }
 }
