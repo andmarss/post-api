7dce809005dbe9114b58f1c8cf09432f54b55498
Андрей Марков <anrey-markov@mail.ru>
2020-03-01 23:49:46 +0300

    правки для валидатора

--- App/System/Validator.php
+++ App/System/Validator.php
@@ -2,6 +2,7 @@
 
 namespace App\System;
 
+use App\System\Database\DB;
 use App\Traits\PluralizeTrait;
 
 final class Validator
@@ -30,38 +31,46 @@ final class Validator
 
                 switch ($rule) {
                     case 'required' && empty(trim($value)):
-                        $this->addError($field,"Поле {$field} обязательно для заполнения");
+                        $this->addError(sprintf('%s.%s', $field, $rule),"Поле {$field} обязательно для заполнения");
                         break;
                     case 'min':
                         if(mb_strlen(trim($value)) < $rule_value) {
-                            $this->addError($field,sprintf("\"{$field}\" должно быть больше {$rule_value} %s.", $this->pluralize(intval($rule_value), ['знака', 'знаков','знаков'])));
+                            $this->addError(sprintf('%s.%s', $field, $rule),sprintf("\"{$field}\" должно быть больше {$rule_value} %s.", $this->pluralize(intval($rule_value), ['знака', 'знаков','знаков'])));
                         }
                         break;
                     case 'max':
                         if(mb_strlen(trim($value)) > $rule_value) {
-                            $this->addError($field,sprintf("\"{$field}\" должно быть меньше {$rule_value} %s.", $this->pluralize(intval($rule_value), ['знака', 'знаков','знаков'])));
+                            $this->addError(sprintf('%s.%s', $field, $rule),sprintf("\"{$field}\" должно быть меньше {$rule_value} %s.", $this->pluralize(intval($rule_value), ['знака', 'знаков','знаков'])));
                         }
                         break;
                     case 'match':
                         if (trim($value) !== $request->{$rule_value}) {
-                            $this->addError($field,"\"{$field}\" должно быть совпадать с {$rule_value}.");
+                            $this->addError(sprintf('%s.%s', $field, $rule),"\"{$field}\" должно быть совпадать с {$rule_value}.");
                         }
                         break;
                     case 'unique':
                         $check = DB::table($rule_value)->where([$field => $value])->first();
 
                         if ($check) {
-                            $this->addError($field,"Пользователь с указанными данными: \"{$value}\", уже существует.");
+                            $this->addError(sprintf('%s.%s', $field, $rule),sprintf('Поле "%s" должно быть уникально. Указанные данные "%s" в таблице "%s" уже существуют', $field, $value, $rule_value));
                         }
+                        break;
+                    case 'exists':
+                        $check = DB::table($rule_value)->where([$field => $value])->first();
+
+                        if (!$check) {
+                            $this->addError(sprintf('%s.%s', $field, $rule),sprintf('Данные с указанным значением "%s=%s" отсутствуют в таблице %s', $field, $value, $rule_value));
+                        }
+
                         break;
                     case 'email':
                         if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                            $this->addError($field,"\"{$field}\" должно быть валидным email-адресом.");
+                            $this->addError(sprintf('%s.%s', $field, $rule),"\"{$field}\" должно быть валидным email-адресом.");
                         }
                         break;
                     case 'int':
                         if (!filter_var((int) $value, FILTER_VALIDATE_INT)) {
-                            $this->addError($field,"\"{$field}\" должно иметь целочисленное значение.");
+                            $this->addError(sprintf('%s.%s', $field, $rule),"\"{$field}\" должно иметь целочисленное значение.");
                         }
                         break;
                 }
@@ -78,7 +87,7 @@ final class Validator
      * Добавляет ошибку в массив по имени поля
      */
 
-    private function addError($item, $error)
+    protected function addError($item, $error)
     {
         if(!isset(static::$errors[$item])) {
             static::$errors[$item] = [];
@@ -93,9 +102,23 @@ final class Validator
      * Получить весь массив ошибок
      */
 
-    protected function getErrors()
+    public function getErrors()
     {
         return static::$errors;
+    }
+
+    public function hasError(string $error)
+    {
+        return isset(static::$errors[$error]);
+    }
+
+    /**
+     * @param string $error
+     * @return mixed|null
+     */
+    public function getError(string $error)
+    {
+        return $this->hasError($error) ? static::$errors[$error] : null;
     }
 
     /**
@@ -113,7 +136,7 @@ final class Validator
      * Проверяет, есть ли ошибки в щапросе
      */
 
-    protected function passed()
+    public function passed()
     {
         return count(static::$errors) === 0;
     }
