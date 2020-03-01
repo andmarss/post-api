f5483f3664e5b616ae86c6f6ec194310d7afe767
Андрей Марков <anrey-markov@mail.ru>
2020-03-01 23:44:12 +0300

    новые поля для config.example.php

--- config.example.php
+++ config.example.php
@@ -7,6 +7,9 @@ return [
             'username'   => '', // имя пользователя
             'password'   => '', // пароль для подключения к БД
             'connection' => '', // тип соединения: "mysql:host=123.4.5.6", "sqlite:example.db" ...etc
+            'charset'    => 'utf8mb4', // кодировка
+            'collation'  => 'utf8mb4_unicode_ci', // представление для таблиц
+            'engine'     => null, // движок
             'options'    => [   //
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
             ]
